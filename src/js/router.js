cf8d17e81615f2e7d9d8b6c91596346488fa6c65
Nekka Amina <amina693@hotmail.fr>
2024-09-30 13:05:18 +0200

    JSDoc fonctions redirect et changeView

--- src/js/router.js
+++ src/js/router.js
@@ -1,3 +1,9 @@
+/**
+ * Redirects the user to a new view.
+ * @param {string} view - The name of the view to redirect to.
+ * @param {boolean} isFromIndex - Whether the current view is the index view.
+ */
+
 const redirect = (view, isFromIndex) => {
   const currentFullPath = window.location.pathname;
   const currentRoute = currentFullPath.split("/").pop();
@@ -13,6 +19,12 @@ const redirect = (view, isFromIndex) => {
   const newPath = `${baseURL}/${view}.html`;
   window.location.href = newPath;
 };
+
+/**
+ * Changes the current view to a new view.
+ * @param {string} view - The name of the view to change to.
+ * @param {boolean} isFromIndex - Whether the current view is the index view.
+ */
 
 const changeView = (view, isFromIndex) => {
   if (view === "login") {
